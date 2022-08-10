a7b63a7a648a43e2cdb4cb02759e54635b034de9
kun yang <kun.yang@pacteraedge.com>
2022-08-10 14:01:01 +0800

    build: 修改配置文件处理index.tsx-eslint报错问题

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
   env: {
     browser: true,
     commonjs: true,
-    es6: true,
+    es6: true
   },
   // 使用的扩展库
   extends: ['plugin:react/recommended', 'airbnb', 'plugin:@typescript-eslint/recommended', 'eslint:recommended'],
@@ -13,23 +13,23 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
     ecmaFeatures: {
-      jsx: true,
-    },
+      jsx: true
+    }
   },
   // 可以全局使用变量
   globals: {
-    React: true,
+    React: true
   },
   settings: {
     'import/parsers': {
-      '@typescript-eslint/parser': ['.ts', '.tsx'],
+      '@typescript-eslint/parser': ['.ts', '.tsx']
     },
     'import/resolver': {
       node: {
         extensions: ['.js', '.ts', '.tsx'],
-        moduleDirectory: ['node_modules', './src'],
-      },
-    },
+        moduleDirectory: ['node_modules', './src']
+      }
+    }
   },
   rules: {
     // 允许在 .js 和 .jsx 文件中使用  jsx
@@ -38,6 +38,7 @@ module.exports = {
     'react/jsx-one-expression-per-line': 0,
 
     indent: 0, // 用于 'plugin:@typescript-eslint/recommended' 后，书写变量 const var let ，eslint报错
-
-  },
+    'comma-dangle': [2, 'never'], // 对象字面量项尾不能有逗号
+    'import/extensions': 'off'
+  }
 };
