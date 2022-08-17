7fb4dd4d295233940364c3136bfdae5a234abd79
kun yang <kun.yang@pacteraedge.com>
2022-08-17 18:01:47 +0800

    feat: 添加Home-lists 页面

--- src/pages/Home/components/lists/index.tsx
+++ src/pages/Home/components/lists/index.tsx
@@ -1,7 +1,57 @@
+import { Button, Card, Col, Row } from 'antd';
+import { useState } from 'react';
+import styles from './lists.module.less';
+
+const ListTitle = () => (
+  <div className={styles.title}>
+    <span datatype='title'>热门海报</span>
+    <span datatype='desc'>只需替换文字和图片，一键自动生成H5</span>
+  </div>
+);
+
+const ListInfo = () => {
+  const [hover, setHover] = useState<number | null>(null);
+  const lists = [1, 2, 3, 4];
+  console.log('------------', hover);
+
+  return (
+    <div className={styles['list-info']}>
+      <Row style={{ width: '100%' }} justify='space-between'>
+        {lists.map((item, index) => (
+          <Col
+            span={4}
+            style={{ position: 'relative', height: '400px' }}
+            onMouseEnter={() => setHover(index)}
+            onMouseLeave={() => setHover(null)}
+          >
+            <Card style={{ width: '100%', height: '100%' }} hoverable>
+              <p>Card content</p>
+              <p>Card content</p>
+              <p>Card content</p>
+            </Card>
+            {hover !== null && index === hover ? (
+              <div className={styles.mantle}>
+                <Button type='primary' shape='round'>
+                  使用该模版创建
+                </Button>
+              </div>
+            ) : null}
+          </Col>
+        ))}
+      </Row>
+    </div>
+  );
+};
+
 const HomeLists = () => {
   console.log('---------HomeLists----------');
 
-  return <div>HomeLists Page</div>;
+  return (
+    <div className={styles.container}>
+      <ListTitle />
+      <ListInfo />
+    </div>
+  );
 };
 
 export default HomeLists;
