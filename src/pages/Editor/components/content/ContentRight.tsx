ae800eb001bd4d0b7ec0d74338e1efec2c78732c
kun yang <kun.yang@pacteraedge.com>
2022-08-19 13:38:33 +0800

    feat: 添加Editor 画布右边区域基本布局

--- src/pages/Editor/components/content/ContentRight.tsx
+++ src/pages/Editor/components/content/ContentRight.tsx
@@ -1,11 +1,24 @@
+import { Tabs } from 'antd';
 import styles from './content.module.less';
+
+const { TabPane } = Tabs;
 
 const ContentRight = () => {
   console.log('-- ContentRight --');
 
   return (
     <div className={`${styles['content-bg']} ${styles['content-size']} `}>
-      ContentRight
+      <Tabs defaultActiveKey='1' type='card'>
+        <TabPane tab='属性设置' key='attribute'>
+          属性设置
+        </TabPane>
+        <TabPane tab='图层设置' key='pageLevel'>
+          图层设置
+        </TabPane>
+        <TabPane tab='页面设置' key='pageSetting'>
+          页面设置
+        </TabPane>
+      </Tabs>
     </div>
   );
 };
