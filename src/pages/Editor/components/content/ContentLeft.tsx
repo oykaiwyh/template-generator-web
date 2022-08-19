b4fdb1be31e32f0a8ff3dc7c6172d457051e342b
kun yang <kun.yang@pacteraedge.com>
2022-08-19 13:33:22 +0800

    feat: 添加Editor 画布左边区域基本布局

--- src/pages/Editor/components/content/ContentLeft.tsx
+++ src/pages/Editor/components/content/ContentLeft.tsx
@@ -1,11 +1,54 @@
+import {
+  BuildOutlined,
+  FieldStringOutlined,
+  FileImageOutlined,
+} from '@ant-design/icons';
+import { Tabs } from 'antd';
 import styles from './content.module.less';
 
+const { TabPane } = Tabs;
 const ContentLeft = () => {
   console.log('-- ContentLeft --');
 
   return (
-    <div className={`${styles['content-bg']} ${styles['content-size']} `}>
-      ContentLeft
+    <div
+      className={`${styles['content-bg']} ${styles['content-size']}  ${styles['left-container']} `}
+    >
+      <Tabs centered>
+        <TabPane
+          tab={
+            <span>
+              <FieldStringOutlined />
+              文本
+            </span>
+          }
+          key='text'
+        >
+          文本
+        </TabPane>
+        <TabPane
+          tab={
+            <span>
+              <FileImageOutlined />
+              图片
+            </span>
+          }
+          key='img'
+        >
+          图片
+        </TabPane>
+        <TabPane
+          tab={
+            <span>
+              <BuildOutlined />
+              形状
+            </span>
+          }
+          key='shap'
+        >
+          形状
+        </TabPane>
+      </Tabs>
     </div>
   );
 };
